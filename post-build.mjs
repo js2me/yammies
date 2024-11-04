283a3fa7fa91b23d6baa48f8d96bba4b68cca274
Sergey Volkov <js2me@outlook.com>
2024-11-04 12:51:43 +0300

    internal: improve publish script

--- post-build.mjs
+++ post-build.mjs
@@ -1,5 +1,4 @@
-import { postBuildScript } from 'js2me-exports-post-build-script';
-
+import { postBuildScript, publishScript } from 'js2me-exports-post-build-script';
 import { $ } from "js2me-exports-post-build-script/utils";
 
 postBuildScript({
@@ -12,6 +11,20 @@ postBuildScript({
     $(`sed -i 's/^export type/type/' dist/util-types.d.ts`);
 
     packageJson.exports['./util-types'] = './util-types.d.ts';
+  },
+  updateVersion: process.env.PUBLISH_VERSION,
+  onPackageVersionChanged: (nextVersion, currVersion) => {
+    if (process.env.PUBLISH) {
+      publishScript({
+        nextVersion,
+        currVersion,
+        publishCommand: 'pnpm publish',
+        commitAllCurrentChanges: true,
+        createTag: true,
+        githubRepoLink: 'https://github.com/js2me/mobx-react-hook-form',
+        cleanupCommand: 'pnpm clean', 
+      })
+    }
   }
 });
 
