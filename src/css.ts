743963eb692746238bc3ad70066bcc33e96dc65b
Sergey Volkov <js2me@outlook.com>
2024-12-30 13:08:59 +0300

    feat: add cx for css utils

--- src/css.ts
+++ src/css.ts
@@ -1,4 +1,12 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 /**
  * Перевод значения в пикселях в rem строковое
  */
 export const toRem = (px: number, remValue = 16) => `${px / remValue}rem`;
+
+/**
+ * classNames/clsx но с примесями tailwind-merge
+ */
+export const cx = (...args: Parameters<typeof clsx>) => twMerge(clsx(...args));
